fb93d7d97cd8b012212cc6e0e6c3ba2814bd13a4
tylerworkman <tyler.workman@uta.edu>
2023-03-06 11:14:28 -0600

    Adding code for sections 3 and 4
    Adding the code snippets for Python variable copying and the terminal-callable script for sections 3 and 4 of hw2

---
+++ hw/hw2/script_call.py
@@ -0,0 +1,5 @@
+from sys import exit
+
+print("This is Python version 3.5.2 \n \n Python is the best language for String manipulation! \n \n !noitalupinam gnirtS rof egaugnal tseb eht si nohtyP \n \n !otlpnmgit o gunlte h inhy \n \n pYTHON IS THE BEST LANGUAGE FOR sTRING MANIPULATION! \n \n \n The sentence 'Python is the best language for String manipulation!' contains \n 4 'a' letters, and \n 0 'A' letters! \n \n Python \n is \n",'the','\n','best','\n','language','\n','for','\n','String','\n','manipulation!','\n','\n','PYTHON','\n','IS','\n','THE','\n','BEST','\n','LANGUAGE','\n','FOR','\n','STRING','\n','MANIPULATION!')
+
+exit()
